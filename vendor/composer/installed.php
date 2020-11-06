84f942f9415ab6ad317c1bc460bc3f388baa9939
Emma van Nieuwburg <535375@student.glu.nl>
2020-11-06 15:45:53 +0100

    2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpstan/phpstan' => 
+    array (
+      'pretty_version' => '0.12.54',
+      'version' => '0.12.54.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '45c7b999a4b7dd9ac5558bdaaf23dcebbef88223',
+    ),
+  ),
+);
